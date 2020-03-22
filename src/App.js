92af18e9602841d7b7877b0b7ce0411224d79340
Eli Johnson <eli9000@gmail.com>
2020-03-21 21:37:38 -0600

    app init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import dude from "./assets/me-dude.jpg";
+import walter from "./assets/me-walter.jpg";
+import "./App.css";
+
+// const dude = import("./assets/me-dude.jpg");
+// const walter = import("./assets/me-walter.jpg");
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <img src={dude} className="img reg" alt="the-dude" />
+        <img src={walter} className="img rev" alt="walter-sobchak" />
+      </header>
+      <div className="App-main">
+        <p>Yeah well, ya know, that's just like, your opinion... man.</p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/eli9000/test-app-ci-cd"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Peep da Repo my People!
         </a>
-      </header>
+      </div>
     </div>
   );
 }
